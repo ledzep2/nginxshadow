f94c8bbd1f378ff83c472de16f117c71ff95a029
Russell <yufeiwu@gmail.com>
2013-04-23 06:00:42 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="Nginx Shadow",
+        version='1.0',
+        author='yufeiwu',
+        author_email='yufeiwu@gmail.com',
+        scripts=['nginxshadow']
+    )
